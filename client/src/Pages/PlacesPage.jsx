df7c6a6ea7f4e094def02163261ec4117af7d894
Davinder Kumar <abhiverma97819@gmail.com>
2023-08-10 19:45:06 +0530

    added filters on image and implemented delete photo functionality

--- client/src/Pages/PlacesPage.jsx
+++ client/src/Pages/PlacesPage.jsx
@@ -44,7 +44,7 @@ const PlacesPage = () => {
                 {/* <PlaceImg place={place} /> */}
                 {place.photos.length > 0 && (
                   <img
-                    className="object-fit"
+                    className="object-cover"
                     src={"http://localhost:5000/uploads/" + place.photos[0]}
                     alt=""
                   />
